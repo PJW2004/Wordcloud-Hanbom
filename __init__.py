6b55833d7234e8fd7147f37f7087409b705da1ee
jeong-won <59524278+PJO2004@users.noreply.github.com>
2022-07-11 21:08:33 +0900

    :bug:

--- __init__.py
+++ __init__.py
@@ -4,11 +4,11 @@ user = input()
 try:
     import numpy as np
     from PIL import Image
-    import defalt
-    wc = defalt.data_create(width=800, hight=800, user=user, mask=img_array)
+    import default
+    wc = default.data_create(width=800, hight=800, user=user, mask=img_array)
     img = Image.open('./sampleImage/img.jpg')
     img_array = np.array(img)
-    defalt.image_show(wc=wc)
+    default.image_show(wc=wc)
 
 except ModuleNotFoundError:
     os.system('pip install -r requirements.txt')
