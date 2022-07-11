0bdd161e4818492cf2978a10364843b8b0278632
jeong-won <59524278+PJO2004@users.noreply.github.com>
2022-07-11 21:07:25 +0900

    :bug:bug testing

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 import os
-import numpy as np
-from PIL import Image
 
 user = input()
-img = Image.open('img.jpg')
-img_array = np.array(img)
 try:
+    import numpy as np
+    from PIL import Image
     import defalt
     wc = defalt.data_create(width=800, hight=800, user=user, mask=img_array)
-
+    img = Image.open('./sampleImage/img.jpg')
+    img_array = np.array(img)
     defalt.image_show(wc=wc)
 
 except ModuleNotFoundError:
